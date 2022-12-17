f4fe1d724f37344481c6b395774144d45b76e553
cyberowl <cybrowl@protonmail.com>
2022-12-17 06:36:52 -0800

    deploy dsign assets working

--- src/heartbeat_assets/routes/+page.svelte
+++ src/heartbeat_assets/routes/+page.svelte
@@ -2,13 +2,14 @@
 	import PageNavigation from 'heartbeat-components/components/PageNavigation.svelte';
 </script>
 
-<!-- Explore -->
 <svelte:head>
 	<title>Heartbeat</title>
 </svelte:head>
 
 <main class="hidden lg:grid grid-cols-12 gap-y-2 relative">
-	<PageNavigation />
+	<div class="col-start-2 col-end-12 mb-8">
+		<PageNavigation navItems={[{ name: 'Canisters', href: '', isSelected: true }]} />
+	</div>
 </main>
 
 <style>
