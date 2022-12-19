0f66b641fa3484ffd549c696d7571c5807119748
cyberowl <cybrowl@protonmail.com>
2022-12-19 02:49:16 -0800

    add: tests

---
+++ test-utils/actor_canister_ids.cjs
@@ -0,0 +1,7 @@
+const canister_ids = require('../.dfx/local/canister_ids.json');
+
+const health_metrics_canister_id = canister_ids.health_metrics.local;
+
+module.exports = {
+	health_metrics_canister_id
+};
---
+++ test-utils/actor_interface.cjs
@@ -0,0 +1,7 @@
+const {
+	idlFactory: health_metrics_interface
+} = require('../.dfx/local/canisters/health_metrics/health_metrics.did.test.cjs');
+
+module.exports = {
+	health_metrics_interface
+};
